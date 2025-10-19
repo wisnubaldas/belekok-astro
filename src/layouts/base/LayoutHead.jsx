877788e1156b0948f975cee27daf2d0bdab84752
wisnuhidayat <wisnubaldas@gmail.com>
2025-10-20 03:21:13 +0700

    belekok astro

--- src/layouts/base/LayoutHead.jsx
+++ src/layouts/base/LayoutHead.jsx
@@ -1,45 +1,28 @@
-import { useState } from 'react';
-export default function LayoutHead({title = "Belekok Astro", description = "Frontend SSR project powered by Astro and React."}) {
-    return (
-        <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-    <meta name="robots" content="noindex, nofollow" />
-    <title>{title}</title>
 
-    <meta name={description} content="" />
+export default function LayoutHead({
+  title = 'Belekok Astro',
+  description = 'Frontend SSR project powered by Astro and React.',
+}) {
+  return (
+    <head>
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+      />
+      <meta name="robots" content="noindex, nofollow" />
+      <title>{title}</title>
 
-    <link rel="icon" type="image/x-icon" href="/assets/img/favicon/favicon.ico" />
+      <meta name="description" content={description} />
 
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&ampdisplay=swap"
-      rel="stylesheet" />
+      <link rel="icon" type="image/x-icon" href="/assets/img/favicon/favicon.ico" />
 
-    <link rel="stylesheet" href="../../assets/vendor/fonts/iconify-icons.css" />
-
-
-    <link rel="stylesheet" href="../../assets/vendor/libs/node-waves/node-waves.css" />
-
-    <script src="../../assets/vendor/libs/@algolia/autocomplete-js.js"></script>
-
-    <link rel="stylesheet" href="../../assets/vendor/libs/pickr/pickr-themes.css" />
-
-    <link rel="stylesheet" href="../../assets/vendor/css/core.css" />
-    <link rel="stylesheet" href="../../assets/css/demo.css" />
-
-
-    <link rel="stylesheet" href="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-
-    <script src="../../assets/vendor/js/helpers.js"></script>
-
-    <script src="../../assets/vendor/js/template-customizer.js"></script>
-
-
-    <script src="../../assets/js/config.js"></script>
-  </head>
-    );
-};
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+        rel="stylesheet"
+      />
+    </head>
+  );
+}
